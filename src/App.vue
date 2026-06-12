7391257c7db5439bd4675fb1b736544874f57b32
Mohsen Ashrafiamiri <ashrafimohsen@ymail.com>
2026-06-12 17:11:45 +0200

    second

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Auth from './pages/Auth.vue';
+import Dashboard from './pages/Dashboard.vue';
+import Nav from './components/Nav.vue'
+
+
 </script>
 
 <template>
-  <HelloWorld />
+
+  <v-app>
+    <v-container>
+      <v-btn>Click Me</v-btn>
+
+      <v-card class="mt-4" width="500">
+        <v-card-title>Hello Vuetify</v-card-title>
+        <v-card-text>
+          If you can see this card, Vuetify is working.
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-app>
+
+  <Auth/>
+
+  <Dashboard/>
+
+  <Nav/>
 </template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <v-app>
+        <v-button v-text="Home"></v-button>
+    </v-app>
+
+</template>
